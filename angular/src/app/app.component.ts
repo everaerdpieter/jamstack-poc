e2d52d819cd235f88c9a4d36cdc4898b59d08055
Pieter <everaerd.pieter@gmail.com>
2020-04-13 14:59:22 +0200

    add form with submit

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'update-github-poc';
+  savedText: string;
+  formGroup: FormGroup;
+
+  constructor() {
+    this.savedText = 'todo: this text should come from the backend';
+  }
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      textArea: new FormControl(null, Validators.required),
+    });
+  }
+
+  submit() {
+    alert(this.formGroup.get('textArea').value);
+  }
 }
